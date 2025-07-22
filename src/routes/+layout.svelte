937e0abb2e766af6d75c0ffe2e45df60e8d81895
heri.appkey <heri.appkey@gmail.com>
2025-07-22 19:21:53 +0800

    Refactor layout component to use a reactive variable for current path in key block

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,16 @@
   import Footer from "../lib/components/Footer.svelte";
   import { fade } from 'svelte/transition';
   import { quintOut } from 'svelte/easing';
+  import { page } from '$app/stores';
+
+  let currentPath: string;
+  $: currentPath = $page.url.pathname;
 </script>
 
 <div class="flex flex-col min-h-screen">
   <Header />
   <main class="flex-grow">
-    {#key $page.url.pathname}
+    {#key currentPath}
       <div in:fade={{ duration: 300, easing: quintOut }} out:fade={{ duration: 300, easing: quintOut }}>
         <slot />
       </div>
